5dab15a69f0f0baa2ed094eacd65b806122cc1cd
Harinandan Teja Katam <hari.katam@gmail.com>
2020-05-26 23:59:31 +0200

    Added color transform utils module

---
+++ src/nnutils/color_transform.py
@@ -0,0 +1,20 @@
+import torch
+
+
+def uv_to_rgb(uv, max_value=None, device='cuda:0'):
+
+    b, _, h, w = uv.shape
+
+    uv_np = uv.detach()
+
+    if max_value is not None:
+        normalized_uv = uv_np / max_value
+    else:
+        normalized_uv = uv_np / (torch.abs(uv_np).max())
+
+    rgb_map = torch.ones((b, 3, h, w), dtype=torch.float).to(device)
+    rgb_map[:, 0] += normalized_uv[:, 0]
+    rgb_map[:, 1] -= 0.5 * (normalized_uv[:, 0] + normalized_uv[:, 1])
+    rgb_map[:, 2] += normalized_uv[:, 1]
+
+    return torch.mul(rgb_map.clamp(0, 1), 255).to(torch.long)
