dba3498aa4186169ab01df66f511a9463800887a
slt <ge75wed@mytum.de>
2020-05-26 15:51:18 +0200

    Fix the problem in UNet

--- src/model/unet.py
+++ src/model/unet.py
@@ -7,15 +7,16 @@ class UNet(nn.Module):
     Module to use UNet with different layers
     """
 
-    def __init__(self, in_channels=3, out_channels=1, features=[32,64,128,256,512]):
+    def __init__(self, in_channels=3, out_channels=1, features=[32,64,128,256,512], batch_norm=False):
         """
         Construct the architecture of UNet
 
         :param in_channels: scalar (default 3 for GRB image)
         :param out_channels: scalar (default 1)
-        :features: list at most 5 elements (default [32,64,128,256,512])
+        :param features: list at most 5 elements (default [32,64,128,256,512])
                    if the list has more than 5 elements, we only keep the first 5
-                   if the list has less than 5 elements, we extend the later layers with double fetures
+                   if the list has less than 5 elements, we extend the later layers with double features
+        ：param batch_norm: bool, whether we have batch normalization (default: false)
         """
         super(UNet, self).__init__()
 
@@ -25,23 +26,23 @@ class UNet(nn.Module):
             while len(features) != 5:
                 features.append(features[-1] * 2)
 
-        self.encoder1 = double_conv(in_channels, features[0])
-        self.encoder2 = double_conv(features[0], features[1])
-        self.encoder3 = double_conv(features[1], features[2])
-        self.encoder4 = double_conv(features[2], features[3])
+        self.encoder1 = double_conv(in_channels, features[0], batch_norm=batch_norm)
+        self.encoder2 = double_conv(features[0], features[1], batch_norm=batch_norm)
+        self.encoder3 = double_conv(features[1], features[2], batch_norm=batch_norm)
+        self.encoder4 = double_conv(features[2], features[3], batch_norm=batch_norm)
 
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.bottleneck = double_conv(features[3], features[4])
+        self.bottleneck = double_conv(features[3], features[4], batch_norm=batch_norm)
 
-        self.decoder4 = double_conv(features[4] + features[3], features[3])
-        self.decoder3 = double_conv(features[3] + features[2], features[2])
-        self.decoder2 = double_conv(features[2] + features[1], features[1])
-        self.decoder1 = double_conv(features[1] + features[0], features[0])
+        self.decoder4 = double_conv(features[4] + features[3], features[3], batch_norm=batch_norm)
+        self.decoder3 = double_conv(features[3] + features[2], features[2], batch_norm=batch_norm)
+        self.decoder2 = double_conv(features[2] + features[1], features[1], batch_norm=batch_norm)
+        self.decoder1 = double_conv(features[1] + features[0], features[0], batch_norm=batch_norm)
         self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
 
         self.conv = nn.Conv2d(
-            in_channels=features, out_channels=out_channels, kernel_size=1
+            in_channels=features, out_channels=out_channels, kernel_size=1, batch_norm=batch_norm
         )
 
     def forward(self, x):
