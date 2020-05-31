ed64f30ae6775df14b3218e3605dc4d750c78d3f
Daniel Scholz <24234559+daniel-scholz@users.noreply.github.com>
2020-05-31 09:20:51 +0200

    Adds probability to single camera pose prediction.

--- src/model/cam_pose_predictor.py
+++ src/model/cam_pose_predictor.py
@@ -33,12 +33,12 @@ class CameraPredictor(nn.Module):
     and translation vector based on an image.
     """
 
-    def __init__(self, device, num_feats, feature_extractor=None):
+    def __init__(self, input_shape, feature_extractor=None):
         """
 
-        :param device: The device on which the computation is performed. Usually CUDA.
         :param feature_extractor: An feature extractor of an image. If None, resnet18 will bes used.
-        :param num_feats: The number of extracted features from the encoder.
+        :param input_shape: Shape of an input image. It is used to calculate the number of neurons
+                            in the FC layer.
         """
         super(CameraPredictor, self).__init__()
         if not feature_extractor:
@@ -50,6 +50,8 @@ class CameraPredictor(nn.Module):
             self.avg_pool = None
         else:
             self.encoder = feature_extractor
+
+        num_feats = 256 * (input_shape[0] // 64) * (input_shape[1] // 64)
 
         self.fc = nn.Linear(num_feats, 256)
         self.fc2 = nn.Linear(256, 8)
@@ -77,9 +79,11 @@ class CameraPredictor(nn.Module):
         # predicted cam_params
         scale = x[..., 0]
         translate = x[..., 1:3 + 1]
-        rotate = x[..., 4:]
+        rotate = x[..., 4:6 + 1]
+        prob = x[..., 7]
+        prob = F.softmax(prob)
 
-        return scale, translate, rotate
+        return scale, translate, rotate, prob
 
 
 class MultiCameraPredictor(nn.Module):
@@ -87,8 +91,8 @@ class MultiCameraPredictor(nn.Module):
 
     # TODO: implement
 
-    def __init__(self):
-        pass
+    def __init__(self, num_hypotheses, input_shape):
+        self.cams = nn.ModuleList([CameraPredictor(input_shape) for _ in range(num_hypotheses)])
 
     def forward(self):
-        pass
+        ...
