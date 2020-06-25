fixed the bug of vertices rotation

src/model/articulation.py
@@ -2,7 +2,7 @@ from pytorch3d.structures import Meshes
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from pytorch3d.transforms import (euler_angles_to_matrix,
+from pytorch3d.transforms import (so3_exponential_map, euler_angles_to_matrix,
                   matrix_to_euler_angles, matrix_to_quaternion,
                   quaternion_multiply, quaternion_to_matrix)
 
@@ -25,7 +25,7 @@ class Predictor(nn.Module):
             nn.LeakyReLU(),
             nn.Linear(num_feats, num_feats),
             nn.LeakyReLU(),
-            nn.Linear(num_feats, num_parts * num_rots)
+            nn.Linear(num_feats, num_parts * (num_rots + num_trans))
         )
         self.axis = nn.Parameter(torch.FloatTensor([1,0,0]).unsqueeze(0).repeat(num_parts, 1).to(device))
         if not axis_move:
@@ -42,9 +42,14 @@ class Predictor(nn.Module):
         vec_tran = vec[... , (self._num_rots):].unsqueeze(-1)
         vec = F.normalize(vec, dim = 2)
         angle = atan2(vec[... ,1], vec[... ,0]).unsqueeze(-1).repeat(1,1,3)
-        self.axis.data = F.normalize(self.axis.unsqueeze(0)).squeeze(0).data
+        self.axis.data = F.normalize(self.axis.unsqueeze(0),dim = -1).squeeze(0).data
         axis = self.axis.unsqueeze(0).repeat(batch_size, 1, 1)
-        R = euler_angles_to_matrix(angle * axis)
+
+        axis = axis.view(-1,3)
+        angle = angle.view(-1,3)
+
+        R = so3_exponential_map(angle * axis)
+        R = R.view(batch_size,num_parts,3,3)
         return R, vec_tran
 
 class Articulation(nn.Module):
@@ -53,19 +58,20 @@ class Articulation(nn.Module):
     It predicts part transforamtion in the form of vertcies coordinate based on an image.
     """
 
-    def __init__(self, mesh : Meshes, parts: list, num_parts: int, rotation_center: torch.Tensor, 
-            parent: dict, num_feats=512, num_rots=2, num_trans = 3, device='cuda'):
+    def __init__(self, mesh : Meshes, parts: list, num_parts: int, rotation_center: dict, 
+            parent: dict, alpha: None, num_feats=512, num_rots=2, num_trans = 3, device='cuda'):
         """
         :param mesh: The base mesh for the certain category.
-        :param parts: Each element in the list represent the part of each vertice in mesh (range:[0, num_parts - 1])
+        :param parts: K element list. Each element in the list represent the part of each vertice in mesh (range:[0, num_parts - 1])
         :param num_parts: The number of parts in mesh.
-        :param rotation_center: The rotation center of each part.
-        :param parent: A dictionary 
+        :param rotation_center: The rotation center of each part. e.g. rotation_center[index0] -> XYZ coordinate of index0
+        :param parent: A dictionary describe the child-parent relationship between parts. e.g. parent[index0] -> index_for_parent_of_index0
+        :param alpha: [K x P] The weight of each vertices for each parts
         :param num_feats:The number of extracted features from the encoder.
         :param num_rots: The number of parameter used in rotation prediction.
         :param num_trans: The number of parameter used in translation prediction.
         :param device: 
-
+            K = number of mesh vertice, P = number of parts
         """
 
         super(Articulation, self).__init__()
@@ -81,14 +87,19 @@ class Articulation(nn.Module):
         self._v_to_p = parts # list: vertice -> part
         # dict: part -> [vertices],  # dict: part -> num   
         self._p_to_v, self._p_n= get_p_to_v(parts, num_parts)    
-        self._r_c = rotation_center
+        self._r_c = get_r_c(rotation_center, num_parts).to(device)
         self._r_c.requires_grad = False
 
 
         self._parent = parent
         self._relationship = get_relationship(parent) # p: [p, parent, grand_parent], no use now
         #tree, [p_1, p_2, ...] if x < y, p_x could not be parent of p_y
-        self._tree, self._order_list = get_tree_order(relationship)
+        self._tree, self._order_list = get_tree_order(parent)
+
+
+        self._alpha = None
+        if alpha:
+            self._alpha = alpha
 
     def forward(self, feats):
         """
@@ -106,27 +117,35 @@ class Articulation(nn.Module):
         batch_size = len(x)
 
         R, t = self._predictor(x)
+
+        #TODO: Add translation for future
         if True:
-            t = tensor.zeros([batch_size, self._num_parts, 3, 1])       
+            t = tensor.zeros([batch_size, self._num_parts, 3, 1]).to(device)       
         
-        
+        t_old = t
+
         rotation_center = self._r_c.unsqueeze(0).unsqueeze(-1).repeat(batch_size,1,1,1)
-        for k in self._order_list:
-            t[:,k,...] += -matmul(R[:,k,...],rotation[:,k,...]) + rotation[:,k,...]
-            if self._parent != -1
-                R[:,k,...] = matmul(R[:,self._parent[k],...],R[:,k,...])
-                t[:,k,...] = matmul(R[:,self._parent[k],...],t[:,k,...]) + t[:,self._num_parts[k],...]
+        for k in _order_list:
+            t[:,k,...] += -torch.bmm(R[:,k,...],rotation_center[:,k,...]) + rotation_center[:,k,...]
+            if self._parent[k] != -1:
+                R[:,k,...] = torch.bmm(R[:,self._parent[k],...],R[:,k,...])
+                t[:,k,...] = torch.bmm(R[:,self._parent[k],...],t[:,k,...]) + t[:,self._parent[k],...]
+
 
         verts = self._mesh.verts_list()[0]
         verts = verts.unsqueeze(0).unsqueeze(-1).repeat(batch_size,1,1,1)
-        for k in self._order_list:
-            ele_k = self._p_to_v[k]
-            verts[:,ele_k,...] =  matmul(R[:,k,...], verts[:,ele_k,...]) + t[:,k,...]
+
+        if self._alpha:
+
+        else:
+            for k in self._order_list:
+                ele_k = self._p_to_v[k]
+                verts[:,ele_k,...] =  torch.matmul(R[:,[k],...], verts[:,ele_k,...]) + t[:,[k],...]
                 
         
         verts = verts.squeeze(-1)
-        t = t.squeeze(-1)
-        loss = t.pow(2).sum(-1).view(-1, self._num_parts).mean(-1)
+        t_old = t_old.squeeze(-1)
+        loss = t_old.pow(2).sum(-1).view(-1, self._num_parts).sum(-1)
 
         return verts, loss
 
@@ -135,7 +154,6 @@ class MultiArticulation(nn.Module):
 
     def __init__(self, num_hypotheses=8, **kwargs):
         """
-
         :param num_hypotheses: number of articulation pose which should be predicted.
         :param kwargs: arguments which are passed through to the single articulation  predictors.
         """
@@ -167,13 +185,6 @@ class MultiArticulation(nn.Module):
         return pred_verts, pred_loss
 
 
-
-    
-
-
-
-                   
-                       
 
 def get_p_to_v( parts : list, num_parts : int):
     parts_info = {}
@@ -220,6 +231,17 @@ def get_tree_order( relationship : dict):
                 order.append(i)
         del tree_list[0]
     return root, order
+
+def get_r_c(rotation_center, num_parts):
+    r_c_list = []
+    assert len(rotation_center) == num_parts
+    for k in range(num_parts):
+        assert k in rotation_center.keys()
+        r_c_list += [rotation_center[k]]
+
+    return torch.Tensor(r_c_list)
+
+
 
 #def axang2quat(angle, axis):
 #    cangle = torch.cos(angle/2)