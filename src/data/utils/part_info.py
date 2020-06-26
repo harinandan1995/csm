ed360f52596a532b3828889e905b3ac06ebb62bb
Peisen <linpeisensh@gmail.com>
2020-06-26 20:17:35 +0200

    documentation partrt_info

--- src/data/utils/part_info.py
+++ src/data/utils/part_info.py
@@ -3,7 +3,6 @@ import xml.etree.ElementTree as ET
 import os.path as osp
 import numpy as np
 from scipy.sparse import csr_matrix, csgraph
-import matplotlib.pyplot as plt
 
 def Part(fids, vids, name):
     '''
@@ -43,8 +42,8 @@ def PartModel(id, fids, vids, children, is_main=False, name='NA'):
 
 def complete_dpm(dpm):
     '''
-
-    :param dpm: a dict contain info of all nodes
+    update dpm to get alpha, boundary and rotation center of each part
+    :param dpm: a dict contain info of mesh
     :return:
     '''
     nodes = dpm['nodes']
@@ -94,9 +93,10 @@ def compute_blend_alpha_m2(verts, nodes, mesh_distances):
     '''
 
     :param verts: array, V * 3
-    :param nodes:
-    :param mesh_distances:
+    :param nodes: list contains Part(dict) info
+    :param mesh_distances: a V * V array, min vertices path distance
     :return:
+    blend_alpha: array, V * P, weights of each vertex for each part
     '''
     nparts = len(nodes)
     blend_alpha = np.zeros((len(verts), nparts))
@@ -109,38 +109,18 @@ def compute_blend_alpha_m2(verts, nodes, mesh_distances):
     blend_alpha = blend_alpha/(1E-12 + blend_alpha.sum(1).reshape(-1,1))
     return blend_alpha
 
-def compute_blend_alpha(verts, parent, child):
-    p_vids = parent['vids']
-    c_vids = child['vids']
-
-    common_vids = set(p_vids).intersection(set(c_vids))
-    common_vids = list(common_vids)
-    p_vids = np.array(p_vids)
-    c_vids = np.array(c_vids)
-    common_vids = np.array(common_vids)
-
-    alpha = np.zeros([len(verts), 2])
-    boundary_verts = verts[common_vids]
-
-    alpha[np.concatenate([p_vids, c_vids]),1] = 1
-    alpha[np.concatenate([p_vids, c_vids]),0] = 1
-
-    for pvid in p_vids:
-        vert = verts[pvid]
-        dist = (boundary_verts - vert.reshape(1,-1))**2
-        dist = np.sqrt(dist.sum(1)).min()
-        alpha[pvid,1] = alpha[pvid,1]*np.exp(-(1*dist))
-
-    for cvid in c_vids:
-        vert = verts[cvid]
-        dist = (boundary_verts - vert.reshape(1,-1))**2
-        dist = np.sqrt(dist.sum(1)).min()
-        alpha[cvid,0] = alpha[cvid,0]*np.exp(-(1*dist))
-    alpha = alpha/(1E-12 + alpha.sum(1).reshape(-1,1))
-    return alpha
-
 
 def compute_rc(verts, parent, child):
+    '''
+
+    :param verts: array, V * 3
+    :param parent: dict. parent node
+    :param child: dict, child node
+    :return:
+    rotation center: array, P * 3
+    boundary vertices: array, N * 3
+
+    '''
     p_vids = parent['vids']
     c_vids = child['vids']
     common_vids = set(p_vids).intersection(set(c_vids))
@@ -198,6 +178,16 @@ def get_parts_from_components(mesh_one, components, names):
 
 
 def create_dpm(allNodes, xmlNode, parts, node_count):
+    '''
+    recursive method to update allNodes
+    :param allNodes: list of parts(nodes)
+    :param xmlNode: Element of parts
+    :param parts: dict contains face and vertex indices for each part
+    :param node_count: int
+    :return:
+    id: int, child node index
+    node_count: int, parent node index
+    '''
     tagName = xmlNode.tag
     part  = parts[tagName]
 
@@ -214,36 +204,19 @@ def create_dpm(allNodes, xmlNode, parts, node_count):
 
 
 
-def save_obj_with_vertex_color(name, results_path, mean_shape):
-    verts  = np.round(mean_shape['verts'],5)
-    faces = mean_shape['faces']
-    vert_colors = np.round(mean_shape['vert_colors'],3)
-    obj_file = osp.join(results_path, '{}.obj'.format(name))
-    with open(obj_file,'w') as f:
-        for vx, v in enumerate(verts):
-            if vert_colors is not None:
-                f.write("v {} {} {} {} {} {}\n".format(v[0], v[1], v[2], vert_colors[vx][0], vert_colors[vx][1], vert_colors[vx][2]))
-            else:
-                f.write("v {} {} {} \n".format(v[0], v[1], v[2]))
-
-        faces = faces + 1
-        for fv in faces:
-            f.write('f {}/{}/ {}/{}/ {}/{}/ \n'.format(fv[0], fv[0], fv[1], fv[1], fv[2], fv[2]))
-    return obj_file
-
-def dump_blend_color_maps(model_dir, nodes, blend_alpha, verts, faces, tone='Reds'):
-    cm = plt.get_cmap(tone)
-    mean_shape = {}
-    mean_shape['verts'] = verts
-    mean_shape['faces'] = faces
-    for nx in range(blend_alpha.shape[1]):
-        name = nodes[nx]['name']
-        vert_colors = cm(blend_alpha[:,nx])
-        mean_shape['vert_colors'] = vert_colors
-        save_obj_with_vertex_color('mem_{}_{}'.format(name, tone), model_dir,
-                                       mean_shape)
-
 def mesh_info(obj_class = 'horse',model_dir = 'datasets/cachedir/imnet/models/horse'):
+    '''
+
+    :param obj_class: string, name of category
+    :param model_dir: string
+    :return:
+    arti_info_mesh: dict contains
+    parts: list, V, parts id of each vertex
+    num_parts: int, P, num of parts
+    rotation_center: array, P * 3, rotation_center of each part
+    parent: dict, parent of each part
+    alpha: array, V * P, weights of vertices for each part
+    '''
     mesh_parts = trimesh.load(osp.join(model_dir, '{}_parts.obj'.format(obj_class)), process=False)
     mesh_one = trimesh.load_mesh(osp.join(model_dir, '{}.obj'.format(obj_class)), process=False)
     hierarchy_xml = osp.join(model_dir, 'hierarchy.xml')
@@ -265,7 +238,6 @@ def mesh_info(obj_class = 'horse',model_dir = 'datasets/cachedir/imnet/models/ho
     complete_dpm(dpm)
 
     ass = dpm['alpha']
-    dump_blend_color_maps(model_dir, dpm['nodes'], dpm['alpha'], verts, faces, tone='Reds')
     part_verts = [-1] * len(verts)
     part_parent = {0:-1}
     part_rotation_center = {}
