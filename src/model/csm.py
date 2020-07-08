Fix the bug for wrong dim in translation in without camera sample

src/model/csm.py
@@ -134,7 +134,7 @@ class CSM(torch.nn.Module):
                     pred_scale.view(-1), pred_trans.view(-1, 2), pred_quat.view(-1, 4)
                 )
                 rotation = rotation.view(batch_size, -1, 3, 3)
-                translation = translation.view(batch_size, -1, 2)
+                translation = translation.view(batch_size, -1, 3)
 
         if self.use_gt_cam or self.use_sampled_cam:
             rotation = rotation.unsqueeze(1)