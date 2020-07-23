512425ffcafbe6f9f384aa7456e5b5a9750549c9
Harinandan Teja Katam <hari.katam@gmail.com>
2020-07-23 10:18:32 +0000

    Moved util function to nnutils Added documentation to UNet

--- src/model/unet.py
+++ src/model/unet.py
@@ -5,103 +5,22 @@ import numpy as np
 import torch
 import torch.nn as nn
 
-
-def upconv2d(in_planes, out_planes, mode='bilinear'):
-    
-    if mode == 'nearest':
-        print('Using NN upsample!!')
-    upconv = nn.Sequential(
-        nn.Upsample(scale_factor=2, mode=mode),
-        nn.ReflectionPad2d(1),
-        nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=1, padding=0),
-        nn.LeakyReLU(0.2,inplace=True)
-    )
-    
-    return upconv
+from src.nnutils.blocks import conv2d, upconv2d, net_init
 
 
-def conv2d(batch_norm, in_planes, out_planes, kernel_size=3, stride=1):
-    
-    if batch_norm:
-        return nn.Sequential(
-            nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=(kernel_size-1)//2, bias=True),
-            nn.BatchNorm2d(out_planes),
-            nn.LeakyReLU(0.2,inplace=True)
-        )
-    else:
-        return nn.Sequential(
-            nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=(kernel_size-1)//2, bias=True),
-            nn.LeakyReLU(0.2,inplace=True)
-        )
-
-
-def bilinear_init(kernel_size=4):
-    # Following Caffe's BilinearUpsamplingFiller
-    # https://github.com/BVLC/caffe/pull/2213/files
-    import numpy as np
-    width = kernel_size
-    height = kernel_size
-    f = int(np.ceil(width / 2.))
-    cc = (2 * f - 1 - f % 2) / (2.*f)
-    weights = torch.zeros((height, width))
-    for y in range(height):
-        for x in range(width):
-            weights[y, x] = (1 - np.abs(x / f - cc)) * (1 - np.abs(y / f - cc))
-
-    return weights
-
-
-def net_init(net):
-    for m in net.modules():
-        if isinstance(m, nn.Linear):
-            #n = m.out_features
-            #m.weight.data.normal_(0, 0.02 / n) #this modified initialization seems to work better, but it's very hacky
-            #n = m.in_features
-            #m.weight.data.normal_(0, math.sqrt(2. / n)) #xavier
-            m.weight.data.normal_(0, 0.02)
-            if m.bias is not None:
-                m.bias.data.zero_()
-
-        if isinstance(m, nn.Conv2d): #or isinstance(m, nn.ConvTranspose2d):
-            #n = m.kernel_size[0] * m.kernel_size[1] * m.in_channels
-            #m.weight.data.normal_(0, math.sqrt(2. / n)) #this modified initialization seems to work better, but it's very hacky
-            m.weight.data.normal_(0, 0.02)
-            if m.bias is not None:
-                m.bias.data.zero_()
-
-        if isinstance(m, nn.ConvTranspose2d):
-            # Initialize Deconv with bilinear weights.
-            base_weights = bilinear_init(m.weight.data.size(-1))
-            base_weights = base_weights.unsqueeze(0).unsqueeze(0)
-            m.weight.data = base_weights.repeat(m.weight.data.size(0), m.weight.data.size(1), 1, 1)
-            if m.bias is not None:
-                m.bias.data.zero_()
-
-        if isinstance(m, nn.Conv3d) or isinstance(m, nn.ConvTranspose3d):
-            #n = m.kernel_size[0] * m.kernel_size[1] * m.kernel_size[2] * m.in_channels
-            #m.weight.data.normal_(0, math.sqrt(2. / n))
-            m.weight.data.normal_(0, 0.02)
-            if m.bias is not None:
-                m.bias.data.zero_()
-
-        elif isinstance(m, nn.BatchNorm2d) or isinstance(m, nn.BatchNorm3d):
-            m.weight.data.fill_(1)
-            m.bias.data.zero_()
-
-
-#------ UNet style generator ------#
-#----------------------------------#
-
-# Defines the Unet generator.
-# |num_downs|: number of downsamplings in UNet. For example,
-# if |num_downs| == 7, image of size 128x128 will become of size 1x1
-# at the bottleneck
-# concats additional features at the bottleneck
 class UNet(nn.Module):
+    """
+    UNet model with skip connections
+    """
     
-    def __init__(self, input_nc, output_nc, num_downs, ngf=64,
-                 norm_layer=nn.modules.normalization.GroupNorm):
-        
+    def __init__(self, input_nc, output_nc, num_downs, ngf=64, norm_layer=nn.modules.normalization.GroupNorm):
+        """
+        :param input_nc: Number of input channels
+        :param output_nc: Number of output channels
+        :param num_downs: Number of down samplings. If |num_downs| == 7, image of size 128x128 will become of size 1x1
+        :param ngf: Number of features of first downsampling. For each downsampling the number of features increase by 2 times.
+        """
+
         super(UNet, self).__init__()
 
         if num_downs >= 5:
@@ -111,7 +30,7 @@ class UNet(nn.Module):
 
         # construct unet structure
         all_blocks = []
-        unet_block = UnetSkipConnectionConcatBlock(ngf_max, ngf_max, input_nc=None, submodule=None, norm_layer=norm_layer, innermost=True)
+        self.inner_most_block = unet_block = UnetSkipConnectionConcatBlock(ngf_max, ngf_max, input_nc=None, submodule=None, norm_layer=norm_layer, innermost=True)
         all_blocks.append(unet_block)
 
 
@@ -143,27 +62,34 @@ class UNet(nn.Module):
         return self.all_blocks
 
 
-# Defines the submodule with skip connection.
-# X -------------------identity---------------------- X
-#   |-- downsampling -- |submodule| -- upsampling --|
 class UnetSkipConnectionConcatBlock(nn.Module):
-    def __init__(self, outer_nc, inner_nc, input_nc=None,
-                 submodule=None, outermost=False, innermost=False, norm_layer=nn.modules.normalization.GroupNorm):
+    """
+    Defines the submodule with skip connection.
+    X -------------------identity---------------------- X
+    |-- downsampling -- |submodule| -- upsampling --|
+    """
+    
+    def __init__(self, outer_nc, inner_nc, input_nc=None, submodule=None, outermost=False, 
+                innermost=False, norm_layer=nn.modules.normalization.GroupNorm):
+        """
+        :param outer_nc: Number of output channels
+        :param input_nv: Number of input channels
+        :param inner_nc: Number of intermediate channels
+        :param submodule: Submodule to be used after down and before up sampling
+        :param outermost: True or False. True if this is the outermost connection in the UNet
+        :param innermost: True of False. True if this is the innermost connection in the UNet
+        """
+        
         super(UnetSkipConnectionConcatBlock, self).__init__()
         self.outermost = outermost
         self.innermost = innermost
 
-        # if submodule is None:
-        #     pdb.set_trace()
         use_bias = norm_layer == nn.InstanceNorm2d
         if input_nc is None:
             input_nc = outer_nc
 
-        downconv = nn.Conv2d(input_nc, inner_nc, kernel_size=4,
-                             stride=2, padding=1, bias=use_bias)
-        downrelu = nn.LeakyReLU(0.01, False)
-        uprelu = nn.ReLU(False)
-
+        downconv = nn.Conv2d(input_nc, inner_nc, kernel_size=4, stride=2, padding=1, bias=use_bias)
+    
         if outermost:
             self.down = [downconv]
             self.up = [upconv2d(inner_nc * 2, inner_nc), nn.Conv2d(inner_nc, outer_nc, kernel_size=3, stride=1, padding=1, bias=True)]
@@ -177,7 +103,6 @@ class UnetSkipConnectionConcatBlock(nn.Module):
         self.up = nn.Sequential(*self.up)
         self.down = nn.Sequential(*self.down)
         self.submodule = submodule
-
 
     def forward(self, x):
         x_inp = x
--- src/nnutils/blocks.py
+++ src/nnutils/blocks.py
@@ -2,6 +2,106 @@ import torch.nn as nn
 import torchvision
 
 
+def upconv2d(in_planes, out_planes, mode='bilinear'):
+    """
+    Upsample + Convolution block
+
+    :param in_planes: Number of input channels
+    :param out_planes: Number of output channels
+    :param mode: Upsample mode
+    """
+    
+    upconv = nn.Sequential(
+        nn.Upsample(scale_factor=2, mode=mode),
+        nn.ReflectionPad2d(1),
+        nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=1, padding=0),
+        nn.LeakyReLU(0.2,inplace=True)
+    )
+    
+    return upconv
+
+
+def conv2d(batch_norm, in_planes, out_planes, kernel_size=3, stride=1):
+    """
+    Convolution + LeakyReLU block
+    
+    :param batch_norm: True or False. True if you want batchnormalizatoin layer
+    :param in_planes: Number of input channels
+    :param out_planes: Number of output channels
+    :param kernel_size: Convolution kernel size
+    :param stride: Convolution kernel stride
+    """
+    
+    if batch_norm:
+        return nn.Sequential(
+            nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=(kernel_size-1)//2, bias=True),
+            nn.BatchNorm2d(out_planes),
+            nn.LeakyReLU(0.2,inplace=True)
+        )
+    else:
+        return nn.Sequential(
+            nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=(kernel_size-1)//2, bias=True),
+            nn.LeakyReLU(0.2,inplace=True)
+        )
+
+
+def bilinear_init(kernel_size=4):
+    # Following Caffe's BilinearUpsamplingFiller
+    # https://github.com/BVLC/caffe/pull/2213/files
+    import numpy as np
+    width = kernel_size
+    height = kernel_size
+    f = int(np.ceil(width / 2.))
+    cc = (2 * f - 1 - f % 2) / (2.*f)
+    weights = torch.zeros((height, width))
+    for y in range(height):
+        for x in range(width):
+            weights[y, x] = (1 - np.abs(x / f - cc)) * (1 - np.abs(y / f - cc))
+
+    return weights
+
+
+def net_init(net):
+    """
+    Initializes model model weights depending on the type of the layer
+
+    :param net: Model for which the weights should be initialized 
+    """
+    
+    for m in net.modules():
+    
+        if isinstance(m, nn.Linear):
+    
+            m.weight.data.normal_(0, 0.02)
+            if m.bias is not None:
+                m.bias.data.zero_()
+
+        if isinstance(m, nn.Conv2d): #or isinstance(m, nn.ConvTranspose2d):
+    
+            m.weight.data.normal_(0, 0.02)
+            if m.bias is not None:
+                m.bias.data.zero_()
+
+        if isinstance(m, nn.ConvTranspose2d):
+    
+            # Initialize Deconv with bilinear weights.
+            base_weights = bilinear_init(m.weight.data.size(-1))
+            base_weights = base_weights.unsqueeze(0).unsqueeze(0)
+            m.weight.data = base_weights.repeat(m.weight.data.size(0), m.weight.data.size(1), 1, 1)
+            if m.bias is not None:
+                m.bias.data.zero_()
+
+        if isinstance(m, nn.Conv3d) or isinstance(m, nn.ConvTranspose3d):
+    
+            m.weight.data.normal_(0, 0.02)
+            if m.bias is not None:
+                m.bias.data.zero_()
+
+        elif isinstance(m, nn.BatchNorm2d) or isinstance(m, nn.BatchNorm3d):
+            m.weight.data.fill_(1)
+            m.bias.data.zero_()
+
+
 def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
     """3x3 convolution with padding"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
